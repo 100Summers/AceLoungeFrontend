be59dc17415f7a3e3fa0a6dbb529a53226356215
Mustafa alhasan <mustafa.alhasan.2020@gmail.com>
2024-02-21 20:37:30 +0300

    Update

---
+++ backend/modles/orders.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const orderItemSchema = new mongoose.Schema({ 
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+    name: String,
+    price: Number,
+    options: [{ name: String, price: Number }]
+});
+
+const orderSchema = new mongoose.Schema({
+    productId,
+    items: [orderItemSchema],
+    tableNumber: Number,
+    orderStatus: { type: String, enum: ['unprocessed', 'processed', 'paid'], default: 'unprocessed' },
+    notes: String
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
